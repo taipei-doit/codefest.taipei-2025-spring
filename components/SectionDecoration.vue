36dbbb3df73ea0fb2e28c2003c1f1cdc90197433
Tanvitsao <tanvitsao@koafaith.com>
2025-03-24 14:39:07 +0800

    feat: completed section decoration

--- components/SectionDecoration.vue
+++ components/SectionDecoration.vue
@@ -9,6 +9,22 @@ const props = withDefaults(
     direction: 'left',
   }
 );
+
+const currentImage = ref(0); // 0 or 1
+
+const toggleImage = () => {
+  currentImage.value = (currentImage.value + 1) % 2;
+};
+
+let intervalId: ReturnType<typeof setInterval>;
+
+onMounted(() => {
+  intervalId = setInterval(toggleImage, 1000); // 每秒切換一次
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
 </script>
 
 <template>
@@ -18,14 +34,30 @@ const props = withDefaults(
   >
     <!-- desktop -->
     <img
+      v-show="currentImage === 0"
       src="/images/section-decoration.svg"
+      class="lg:block hidden w-[750px]"
+      :class="props.direction === 'right' ? 'transform scale-x-[-1]' : ''"
+      alt=""
+    />
+    <img
+      v-show="currentImage === 1"
+      src="/images/section-decoration-2.svg"
       class="lg:block hidden w-[750px]"
       :class="props.direction === 'right' ? 'transform scale-x-[-1]' : ''"
       alt=""
     />
     <!-- mobile -->
     <img
+      v-show="currentImage === 0"
       src="/images/section-decoration-mobile.svg"
+      class="lg:hidden block"
+      :class="props.direction === 'right' ? 'transform scale-x-[-1]' : ''"
+      alt=""
+    />
+    <img
+      v-show="currentImage === 1"
+      src="/images/section-decoration-mobile-2.svg"
       class="lg:hidden block"
       :class="props.direction === 'right' ? 'transform scale-x-[-1]' : ''"
       alt=""
