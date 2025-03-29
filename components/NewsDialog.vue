2794e705ee79bda963fa0205aba29e540c20adfe
Tanvitsao <tanvitsao@koafaith.com>
2025-03-29 10:55:25 +0800

    feat: add news 2

--- components/NewsDialog.vue
+++ components/NewsDialog.vue
@@ -37,7 +37,7 @@ const emit = defineEmits(['close']);
           class="flex-1 overflow-y-auto px-6 pb-6 text-lg leading-8 whitespace-pre-wrap"
         >
           <img v-if="activeNews?.image_url" :src="activeNews.image_url" alt="" class="mb-4" />
-          {{ activeNews?.content }}
+          <div v-html="activeNews?.content"></div>
           <!-- 占位 padding (可選) -->
           <div class="h-6"></div>
         </DialogDescription>
