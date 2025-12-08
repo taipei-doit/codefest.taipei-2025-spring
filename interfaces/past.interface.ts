e4e8e99f03a65b6f8513050d5f311436145d18f4
stanleyfish0813 <stanley.fish.0813@gmail.com>
2025-12-08 02:00:47 +0000

    refactor: 提取多個介面的共同屬性至基礎介面，減少重複定義。

--- interfaces/past.interface.ts
+++ interfaces/past.interface.ts
@@ -1,14 +1,20 @@
 /**
+ * 過去回顧項目的共同屬性
+ */
+export interface PastReviewCommonProps {
+  id: number;
+  /** 回顧縮圖 */
+  thumbnail: string;
+}
+
+/**
  * 獲獎團隊
  */
-export interface PastWinningTeam {
-  id: number;
+export interface PastWinningTeam extends PastReviewCommonProps {
   /** 名次 */
   ranking: string;
   /** 團隊名稱 */
   team_name: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 團隊成員 */
   team_members: string;
   /** 團隊簡介 */
@@ -23,12 +29,9 @@ export interface PastWinningTeam {
 /**
  * 照片回顧
  */
-export interface PastPhoto {
-  id: number;
+export interface PastPhoto extends PastReviewCommonProps {
   /** 回顧名稱 */
   title: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 多圖連結 */
   image_list: string[];
   /** 更多照片連結 */
@@ -38,14 +41,11 @@ export interface PastPhoto {
 /**
  * 參賽影音回顧
  */
-export interface PastVideo {
-  id: number;
+export interface PastVideo extends PastReviewCommonProps {
   /** 日期 */
   date: string;
   /** 回顧名稱 */
   title: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 回顧影片連結 */
   video_url: string;
 }
