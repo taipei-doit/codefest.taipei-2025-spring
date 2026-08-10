eda12564636b393e338cd501b1313048dd37cc81
stanleyfish0813 <stanley.fish.0813@gmail.com>
2026-08-10 02:00:27 +0000

    refactor: 簡化滾動偵測邏輯，使用布林值直接賦值以提升可讀性。

--- app.vue
+++ app.vue
@@ -6,27 +6,23 @@ const { activeDialog } = storeToRefs(dialogStore);
 
 const footerTop = ref(0);
 const scrollY = ref(0);
-
 const isAboveFooter = ref(false);
 
 const updateScroll = () => {
-  scrollY.value = window.scrollY; // 取得目前的滾動位置
+  scrollY.value = window.scrollY;
 
   const footer = document.getElementById('footer');
   if (footer) {
-    footerTop.value = footer.getBoundingClientRect().top + window.scrollY; // 計算 Footer 頂部的 scrollY
+    footerTop.value = footer.getBoundingClientRect().top + window.scrollY;
   }
 
-  if (scrollY.value + window.innerHeight >= footerTop.value) {
-    isAboveFooter.value = true;
-  } else if (scrollY.value + window.innerHeight < footerTop.value) {
-    isAboveFooter.value = false;
-  }
+  const viewportBottom = scrollY.value + window.innerHeight;
+  isAboveFooter.value = viewportBottom >= footerTop.value;
 };
 
 onMounted(() => {
   window.addEventListener('scroll', updateScroll);
-  updateScroll(); // 初始化
+  updateScroll();
 });
 
 onUnmounted(() => {
@@ -35,7 +31,6 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <!-- Google Tag Manager (noscript) -->
   <noscript
     ><iframe
       src="https://www.googletagmanager.com/ns.html?id=GTM-MQCC8H3V"
@@ -45,7 +40,6 @@ onUnmounted(() => {
       title="Google Tag Manager"
     ></iframe
   ></noscript>
-  <!-- End Google Tag Manager (noscript) -->
   <div class="container mx-auto">
     <OrganismLayoutHeader
       id="header"
@@ -53,10 +47,8 @@ onUnmounted(() => {
     />
     <NuxtPage class="lg:mt-[100px] mt-[80px]" />
   </div>
-  <!-- control bar -->
   <OrganismControlBar :is-above-footer="isAboveFooter" />
   <OrganismLayoutFooter id="footer" :class="isAboveFooter ? 'mt-4' : 'mt-[162px]'" />
 
-  <!-- 報名表單 -->
   <OrganismApplyDialog :is-open="activeDialog === 'apply'" @close="dialogStore.closeDialog()" />
 </template>
